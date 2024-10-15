16e227c237d297e136a4f9ea1bb784b7e22fd540
shezanusman08 <164097373+shezanusman08@users.noreply.github.com>
2024-10-16 01:49:12 +0500

    fixed header

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -16,13 +16,7 @@ export default function Header() {
              className="absolute inset-0 w-full h-full z-0"
             />
           </div>
-          <h1 className="text-[#461111] text-4xl md:text-6xl lg:text-7xl font-bold absolute top-16 right-10 transition-transform duration-300 hover:scale-105">
-          Welcome to Hashtag Caffè
-        </h1>
-        <p className="text-[#461111] text-lg md:text-xl lg:text-2xl absolute top-36 right-10 transition-transform duration-300 hover:scale-105">
-          Your favorite coffee place
-        </p>
-
+          
                 
         </div>
 
